3eef186ae8cf27060f1b40432fc4c891cd20aec2
random-debug <112684637+pham-vinh@users.noreply.github.com>
2022-10-05 19:17:15 -0700

    Created TicTacToe Outline

---
+++ tictactoe.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// boolean of either pvp or pvc
+
+//declare method displayMenu()
+
+int main()
+{
+	
+
+	// method for displaying menu
+	// get input on menu
+	// check if input is valid and go to the game of choice
+	
+	// Long String Matrix? or Array2D of Chars?
+
+	// print out the first game state via method in a while loop while getting input
+	// if pvc get random space and input in. check if the spot is open or not.
+	// similar to pvp check if spot is open. Method here()
+	// in gameState method check if the game is over or not.
+	
+
+	// ***Make sure to document throughout methods***
+
+
+	return 0;
+}
+
+//void displayMenu(int input)
+
+//boolean checkGameState()
+
+//void displayGameState()
+
+//computerInput()
+
