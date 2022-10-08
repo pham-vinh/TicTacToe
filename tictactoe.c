d71d9eda64ef3f6a5127e6cd7453fa2e1846bd7b
random-debug <112684637+pham-vinh@users.noreply.github.com>
2022-10-07 20:21:28 -0700

    Added Diagonal Check for Win

--- tictactoe.c
+++ tictactoe.c
@@ -102,22 +102,18 @@ int isGameOver()
 		return 1;
 
 	// Rows and Columns
-	
-	if ((board[0][0] != 0 && board[0][0] == board[0][1] && board[0][1] == board[0][2]) || (board[0][0] != 0 && board[0][0] == board[1][0] && board[1][0] == board[2][0])) {
+	if ((board[0][0] != 0 && board[0][0] == board[0][1] && board[0][1] == board[0][2]) || (board[0][0] != 0 && board[0][0] == board[1][0] && board[1][0] == board[2][0]))
 		return 1;
-	}
 	else if ((board[1][0] != 0 && board[1][0] == board[1][1] && board[1][1] == board[1][2]) || (board[0][1] != 0 && board[0][1] == board[1][1] && board[1][1] == board[2][1]))
-	{
 		return 1;
-	}
 	else if ((board[2][0] != 0 && board[2][0] == board[2][1] && board[2][1] == board[2][2]) || (board[0][2] != 0 && board[0][2] == board[1][2] && board[1][2] == board[2][2]))
-	{
 		return 1;
-	}
 
-	
-	
-	return 0; // false
+	// Diagonals
+	else if ((board[0][0] != 0 && board[0][0] == board[1][1] && board[2][2]) || (board[0][2] != 0 && board[0][2] == board[2][2] && board[2][2] == board[2][0]))
+		return 1;
+	else
+		return 0; // false
 }
 
 // get coords and swap
